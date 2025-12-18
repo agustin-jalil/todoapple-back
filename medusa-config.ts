b0ea5453ca63cd1feb6223beaa07c470c45d1708
Agustin Jalil Leon <115709245+agustin-jalil@users.noreply.github.com>
2025-12-18 00:29:07 -0300

    Remove default port setting in medusa-config
    Remove default port value from configuration.

--- medusa-config.ts
+++ medusa-config.ts
@@ -6,7 +6,6 @@ module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     http: {
-      port: Number(process.env.PORT) || 9000,
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
